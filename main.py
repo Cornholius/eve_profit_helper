aebc23f9822b9114f6fcb12efcda8444aa5f60d2
lyi <y.layshkin@gmail.com>
2021-10-22 16:04:54 +0300

    промежуточный, надо прикрутить rtfm к кнопке

--- main.py
+++ main.py
@@ -28,9 +28,10 @@ class MyHandler(FileSystemEventHandler):
     def __init__(self):
         self.count = [0]
 
-    def on_any_event(self, event):
+    def on_modified(self, event):
         self.count.append(os.path.getsize(event.src_path))
         if self.count[-1] == self.count[-2]:
+            self.count = [0]
             mainWindow.get_values(event.src_path)
 
 
@@ -52,23 +53,31 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi('mainWindow.ui', self)
+        self.sell = self.buy = self.profit = 0
+        self.file_with_prices = []
         self.settings_btn.clicked.connect(SettingsWindow.show_window)
         # self.on_error.setText(' '.join(check.settings['error']))
-        # self.copy_sell_price_btn.clicked.connect(self.copy_to_clopboard('sell'))
-        # self.copy_buy_price_btn.clicked.connect(self.copy_to_clopboard('buy'))
+        self.copy_sell_price_btn.clicked.connect(lambda: self.copy_to_clopboard(self.sell))
+        self.copy_buy_price_btn.clicked.connect(lambda: self.copy_to_clopboard(self.buy))
 
+    def copy_to_clopboard(self, data):
+        QApplication.clipboard().setText(str(data))
 
-    def copy_to_clopboard(self, btn):
-        if btn == 'sell':
-            QApplication.clipboard().setText(self.sell_price_value.text())
-        else:
-            QApplication.clipboard().setText(self.buy_price_value.text)
+    def find_prices_in_file(self, price_type, radius):
+        for i in self.file_with_prices:
+            line = i.split(",")
+            if line[7] == price_type and int(line[-2]) == radius:
+                if price_type == 'False':
+                    self.sell = float(line[0])
+                else:
+                    self.buy = float(line[0])
+                break
 
-    def set_values(self, sell, buy, profit):
-        self.sell_price_value.setText(f'{sell:,.2f}'.replace(',', ' '))
-        self.buy_price_value.setText(f'{buy:,.2f}'.replace(',', ' '))
-        self.profit_value.setText(f'{profit:,.2f}'.replace(',', ' '))
-        if profit < 0:
+    def set_values(self, sell_price_with_bid, buy_price_with_bid):
+        self.sell_price_value.setText(f'{float(sell_price_with_bid):,.2f}'.replace(',', ' '))
+        self.buy_price_value.setText(f'{float(buy_price_with_bid):,.2f}'.replace(',', ' '))
+        self.profit_value.setText(f'{self.profit:,.2f}'.replace(',', ' '))
+        if self.profit < 0:
             self.profit_value.setStyleSheet('color: #cc0000')
         else:
             self.profit_value.setStyleSheet('color: #008000')
@@ -76,28 +85,18 @@ class MainWindow(QtWidgets.QMainWindow):
     # Получаем значения sell и buy ордеров и добавляем шаг ставки
     def get_values(self, file):
         with open(file, 'r') as f:
-            lines = f.readlines()
-            # sell = float(lines[1].split(',')[0])
-            for i in lines:
-                line = i.split(",")
-                # print(line)
-                if line[7] == 'False' and line[-2] == '0':
-                    sell = float(line[0])
-                    break
-            for i in lines:
-                if re.search(r'\bTrue\b', i):
-                    buy = float(i.split(',')[0])
-                    break
-
-        str_sell_price = format(sell, '.2f')[:5]
-        str_buy_price = format(buy, '.2f')[:5]
+            self.file_with_prices = f.readlines()
+        self.find_prices_in_file('False', check.settings['sell_radius'])
+        self.find_prices_in_file('True', check.settings['buy_radius'])
+        str_sell_price = format(self.sell, '.2f')[:5]
+        str_buy_price = format(self.buy, '.2f')[:5]
         sell_price_with_bid = 0
         buy_price_with_bid = 0
 
         # если цена выше 10000
         if '.' not in str_sell_price and '.' not in str_buy_price:
-            tail_of_sell_price = len(str(int(sell))) - 4
-            tail_of_buy_price = len(str(int(buy))) - 4
+            tail_of_sell_price = len(str(int(self.sell))) - 4
+            tail_of_buy_price = len(str(int(self.buy))) - 4
             str_sell_price, str_buy_price = str_sell_price[:4], str_buy_price[:4]
             sell_price_with_bid = str(int(str_sell_price) - 1) + '0' * tail_of_sell_price
             buy_price_with_bid = str(int(str_buy_price) + 1) + '0' * tail_of_buy_price
@@ -115,16 +114,16 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # если цена от 1 до 100
         if '.' in str_sell_price[:3] and '.' in str_buy_price[:3]:
-            sell_price_with_bid = sell + 0.01
-            buy_price_with_bid = buy + 0.01
+            sell_price_with_bid = self.sell + 0.01
+            buy_price_with_bid = self.buy + 0.01
 
         sell_broker = (float(sell_price_with_bid) / 100) * float(check.settings['broker_tax'])
         buy_broker = (float(buy_price_with_bid) / 100) * float(check.settings['broker_tax'])
         sell_tax = (float(sell_price_with_bid) / 100) * float(check.settings['sell_tax'])
-        sell_final = sell - sell_tax - sell_broker
-        buy_final = buy + buy_broker
-        profit = sell_final - buy_final
-        self.set_values(float(sell_price_with_bid), float(buy_price_with_bid), profit)
+        sell_final = self.sell - sell_tax - sell_broker
+        buy_final = self.buy + buy_broker
+        self.profit = sell_final - buy_final
+        self.set_values(sell_price_with_bid, buy_price_with_bid)
 
 
 class SettingsWindow(QtWidgets.QMainWindow):
@@ -136,6 +135,8 @@ class SettingsWindow(QtWidgets.QMainWindow):
         self.market_logs_btn.clicked.connect(self.find_logs_path)
         self.sell_tax_value.setValue(check.settings['sell_tax'])
         self.broker_tax_value.setValue(check.settings['broker_tax'])
+        self.sell_order_radius_count.setValue(check.settings['sell_radius'])
+        self.buy_order_radius_count.setValue(check.settings['buy_radius'])
         self.on_top_checkBox.setChecked(bool(check.settings['always_on_top']))
         self.market_logs_path.setText(check.settings['logs_path'])
         self.opacity_slider.setRange(10, 100)
@@ -163,6 +164,8 @@ class SettingsWindow(QtWidgets.QMainWindow):
         data = check.load_settings()
         data['broker_tax'] = self.broker_tax_value.value()
         data['sell_tax'] = self.sell_tax_value.value()
+        data['sell_radius'] = self.sell_order_radius_count.value()
+        data['buy_radius'] = self.buy_order_radius_count.value()
         data['always_on_top'] = self.on_top_checkBox.isChecked()
         data['opacity'] = float(self.opacity_slider.value() / 100)
         check.save_settings(data)
